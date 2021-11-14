541262683a916bd8927b237031f7725785a5cb1a
Arturo Zambrano <turoar2006@gmail.com>
2021-11-15 00:44:57 +0100

    Arreglado un bug donde se tenia que dar dos veces a login en la parte de rider para iniciar sesion. Tambien se agrego LazyLoad en App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,34 @@
 import { Switch, Route, Link } from 'react-router-dom';
-import Orders from './components/pages/Orders';
-import Rider from './components/pages/Rider';
-import RiderLogin from './components/pages/RiderLogin';
+import React, { Suspense } from 'react';
+
+// import Orders from './components/pages/Orders';
+// import Rider from './components/pages/Rider';
+// import RiderLogin from './components/pages/RiderLogin';
+
+const Orders = React.lazy(() => import('./components/pages/Orders'));
+const Rider = React.lazy(() => import('./components/pages/Rider'));
+const RiderLogin = React.lazy(() => import('./components/pages/RiderLogin'));
 
 function App() {
 	return (
-		<Switch>
-			<Route path='/' exact>
-				<Link to='/rider'>Login rider</Link>
-			</Route>
-			<Route path='/orders'>
-				<Orders />
-			</Route>
-			<Route path='/rider' exact>
-				<RiderLogin />
-			</Route>
-      <Route path="/rider/:riderId">
-        <Rider />
-      </Route>
-		</Switch>
+		<Suspense fallback={
+			<p>Loading ...</p>
+		}>
+			<Switch>
+				<Route path='/' exact>
+					<Link to='/rider'>Login rider</Link>
+				</Route>
+				<Route path='/orders'>
+					<Orders />
+				</Route>
+				<Route path='/rider' exact>
+					<RiderLogin />
+				</Route>
+				<Route path='/rider/:riderId'>
+					<Rider />
+				</Route>
+			</Switch>
+		</Suspense>
 	);
 }
 
--- client/src/components/pages/RiderLogin.js
+++ client/src/components/pages/RiderLogin.js
@@ -9,28 +9,26 @@ import { getRiderByCode } from '../lib/api';
 const RiderLogin = props => {
 	const history = useHistory();
 	const codeRef = useRef();
-	const {sendRequest, status, data} = useHttp(getRiderByCode, true);
+	const { sendRequest, status, data } = useHttp(getRiderByCode, true);
 
 	const submitHandler = async event => {
 		event.preventDefault();
-
 		const code = codeRef.current.value.trim();
-		
-		await sendRequest({code: code});
-		
-		if(status === 'completed'){
-			history.push(`/rider/${data.result._id}`);
-			localStorage.setItem('rider', JSON.stringify(data.result));
-		}
-
+		await sendRequest({ code: code });
 	};
+
+	if (status === 'completed') {
+		history.push(`/rider/${data.result._id}`);
+		localStorage.setItem('rider', JSON.stringify(data.result));
+	}
+
 	return (
 		<Fragment>
 			<h2>Introduce tu código</h2>
 			<Form onSubmit={submitHandler}>
 				<Form.Group className='mb-3' controlId='formGridAddress1'>
 					<Form.Label>Código</Form.Label>
-					<Form.Control ref={codeRef} required/>
+					<Form.Control ref={codeRef} required />
 				</Form.Group>
 				<Button type='submit'>Log-in</Button>
 			</Form>
