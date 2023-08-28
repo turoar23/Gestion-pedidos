5ccd6e23e6ea63ef3d2e4803f17f29cfac8f211e
Arturo Zambrano <turoar23@gmail.com>
2023-08-28 21:16:42 +0200

    Fix: update source

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -2,13 +2,16 @@
 
 echo "Start uploading to azape"
 
-echo "Step 1 - Compress"
+echo "step 1 - build"
+yarn run build
+
+echo "Step 2 - Compress"
 zip -r upload.zip . -x node_modules/**\* -x client/node_modules/**\*
 
-echo "Step 2 - Send zip"
+echo "Step 3 - Send zip"
 scp upload.zip server@azape.es:~
 
-echo "Step 3 - clean directoy"
+echo "Step 4 - clean directoy"
 rm upload.zip
 
 echo "Upload completed"
